2a1418c00b9738619bfd5e98b850ad1b3f5bc775
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-04-29 15:12:03 -0500

    15) feat: requirement 2 of sprint 3 done with it's own test

--- app/Views/UserView.js
+++ app/Views/UserView.js
@@ -3,7 +3,14 @@ const userService = require("../../app/services/UserService");
 class UserView{
 
     static createUser(payload){
-        if(payload === null) return { error: "payload no existe"}
+        let error = "";
+        if(payload === null){
+          error = "payload no existe";
+        } else if(Object.values(payload).some(value => value === null)){
+            error = "necesitan tener un valor válido";
+        }
+
+        return { error: error}
     }
 
 }
--- test/Views/UserView.test.js
+++ test/Views/UserView.test.js
@@ -8,4 +8,12 @@ describe("Tests for User View", () => {
         const create_User = UserView.createUser(payload);
         expect(create_User.error).toMatch(/payload no existe/);
     });
+
+    //2. Valida que un `payload` contenga en alguna de las llaves `username`, `name`, o `id` un valor en `null`. Si hay un valor `null` regresa un objeto con la llave `error` y que indique el texto: `necesitan tener un valor válido`.
+
+    test("2) Return an error if some of Payload's keys contains a null value", () => {
+        const payload = {id: 2, username: null, name: "Didí"};
+        const createUser = UserView.createUser(payload);
+        expect(createUser.error).toMatch(/necesitan tener un valor válido/);
+    });
 });
